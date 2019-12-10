3be250dd21430c00a505c43e03506aa4dfc641fb
Carla Cauche <58265171+carlacch@users.noreply.github.com>
2019-12-10 19:40:07 +0100

    Singleton pattern for BoardGame.cs

--- Monopoly/BoardGame.cs
+++ Monopoly/BoardGame.cs
@@ -8,19 +8,21 @@ namespace Monopoly
 {
     class BoardGame
     {
-        private readonly int size = 40;
+        private static BoardGame instance;
+        private static readonly int size = 40;
         private List<Box> squares = new List<Box>(40);
 
+        /*
         public BoardGame(int size, List<Box> squares)
         {
             this.size = size;
             this.squares = squares;
-        }
+        }*/
 
+        /*n'est pas utile pour le jeu, la taille se récupèrer seulement dans cette classe*/
         public int Size
         {
             get { return size; }
-            
         }
 
         public List<Box> Squares
@@ -45,7 +47,7 @@ namespace Monopoly
 
         private void build(int i)
         {
-            //Box s = new Box("Square " + i, i - 1);
+            //Box s = new Box("Box " + i, i - 1);
             //squares.add(s);
         }
 
